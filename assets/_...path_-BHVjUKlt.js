ae7fe96877329290cec091ebd0c3019d23567438
YunYouJun <me@yunyoujun.cn>
2024-06-04 06:49:44 +0000

    Deploying to gh-pages from @ valaxyjs/valaxy-theme-gitlink@7fe1591be3e8d767e1a5d8829927ccb1c0b8113e 🚀

---
+++ assets/_...path_-BHVjUKlt.js
@@ -0,0 +1,1 @@
+import{d as s,h as a,i as n,j as r,o as c,c as p}from"./app-B2YxGJaF.js";import{b as o}from"./route-block-B_A1xBdJ.js";const u=s({__name:"[...path]",setup(i){const t=a(),e=n();return r(()=>{t.push("/"),e.path!=="/"&&e.path.endsWith("/")&&t.replace(e.path.slice(0,-1))}),(f,h)=>(c(),p("div"))}});typeof o=="function"&&o(u);export{u as default};
--- assets/post-cda6d28a.js
+++
@@ -1,1 +0,0 @@
-import{_ as c,c as t,o as e,L as n}from"./app-a8ce870e.js";const s={};function a(r,_){const o=n;return e(),t(o)}const p=c(s,[["render",a]]);export{p as default};
