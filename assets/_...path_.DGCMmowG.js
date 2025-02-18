61d75fb4e580635030f71f9d801f4c716c206117
YunYouJun <me@yunyoujun.cn>
2025-02-18 18:59:12 +0000

    Deploying to gh-pages from @ valaxyjs/valaxy-theme-gitlink@be82d2406e54c3d063ec0afd2df07b7256d1c9bd 🚀

---
+++ assets/_...path_.DGCMmowG.js
@@ -0,0 +1,1 @@
+import{u as a,d as s,e as t}from"./chunks/vue-router.CYCo_fEr.js";import{j as n,E as r,N as c,L as p}from"./framework.Cn6PPJXy.js";const u=n({__name:"[...path]",setup(f){const o=s(),e=a();return r(()=>{e.path!=="/"&&e.path.endsWith("/")&&o.replace(e.path.slice(0,-1))}),(i,m)=>(p(),c("div"))}});typeof t=="function"&&t(u);export{u as default};
